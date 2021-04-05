c3264b13f641f37c7f41d16d12ac443edef94d47
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-04-05 15:14:26 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,7 +1,56 @@
-// Copyright 2021 GHA Test Team
+//  Copyright Baklanov 2021
 #include <gtest/gtest.h>
 #include <string>
+#include <iostream>
 #include "train.h"
 
+TEST(Train, TEST1) {
+    int i = 0;
+    Train* newTrain = new Train();
+    Cage* newCage = new Cage();
+    newCage->on();
+    newTrain->addCage(newCage);
+    EXPECT_EQ(1, newTrain->get());
+}
 
+TEST(Train, TEST2) {
+    int i = 0;
+    int cageMount = 0;
+    Train* newTrain = new Train();
+    for (i = 0; i < cageMount; ++i) {
+        Cage* newCage = new Cage();
+        if (std::rand() % 2) {
+            newCage->on();
+        }
+        newTrain->addCage(newCage);
+    }
+    EXPECT_EQ(0, newTrain->countLength());
+}
 
+TEST(Train, TEST3) {
+    int i = 0;
+    int cageMount = 1;
+    Train* newTrain = new Train();
+    for (i = 0; i < cageMount; ++i) {
+        Cage* newCage = new Cage();
+        if (std::rand() % 2) {
+            newCage->on();
+        }
+        newTrain->addCage(newCage);
+    }
+    EXPECT_EQ(1, newTrain->countLength());
+}
+
+TEST(Train, TEST4) {
+    int i = 0;
+    int cageMount = 778;
+    Train* newTrain = new Train();
+    for (i = 0; i < cageMount; ++i) {
+        Cage* newCage = new Cage();
+        if (std::rand() % 2) {
+            newCage->on();
+        }
+        newTrain->addCage(newCage);
+    }
+    EXPECT_EQ(778, newTrain->countLength());
+}
