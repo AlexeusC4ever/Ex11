21cf9a7140761e2a5d323bffd2c25fe4727a6fb8
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-04-05 15:12:17 +0300

    Add files via upload

---
+++ include/train.h
@@ -0,0 +1,30 @@
+﻿// Copyright Baklanov 2021
+#ifndef INCLUDE_TRAIN_H_
+#define INCLUDE_TRAIN_H_
+
+class Cage {
+private:
+    bool light;
+
+public:
+    Cage* next;
+    Cage* prev;
+    Cage() : light(false), next(nullptr), prev(nullptr) {}
+    void on() { light = true; }
+    void off() { light = false; }
+    bool get() const { return light; }
+};
+
+class Train {
+ private:
+    Cage* first;
+    Cage* last;
+
+ public:
+    Train():
+        first(nullptr), last(nullptr) {}
+    void addCage(Cage* cage);
+    void printCagesCond();
+    int countLength();
+};
+#endif  // INCLUDE_TRAIN_H_
