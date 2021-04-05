da874618620ab84524d148f9db6037185d8546d4
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-04-05 15:13:01 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+// Copyright Baklanov 2021
+#include "train.h"
+#include <iostream>
+
+int main() {
+    int i = 0;
+    int cageMount = 778;
+    Train* newTrain = new Train();
+    for (i = 0; i < cageMount; ++i) {
+        Cage* newCage = new Cage();
+        if (std::rand() % 2) {
+            newCage->on();
+        }
+        newTrain->addCage(newCage);
+    }
+    std::cout << newTrain->countLength() << std::endl;
+}
